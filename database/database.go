7dd3ee00709fbdae8a3fd04e4352f4cbae9b49b5
HoronLee <horonlee@foxmail.com>
2024-11-10 22:17:10 +0800

    optimize(database): 删除部分无用代码

--- database/database.go
+++ database/database.go
@@ -14,46 +14,46 @@ type Connector interface {
 }
 
 func CheckAndSetDefaultConfig() {
-	// 如果 Database.Type 未设置，默认使用 sqlite
-	if config.Data.Database.Type == "" {
-		config.Data.Database.Type = "sqlite"
+	// 如果 Type 未设置，默认使用 sqlite
+	if config.Data.Type == "" {
+		config.Data.Type = "sqlite"
 		logger.GlobalLogger.Warn("Database type is not specified, defaulting to sqlite")
 	}
-	// 对于 SQLite，如果 Database.Path 未设置，则设置为当前目录下的 data.db
-	if config.Data.Database.Type == "sqlite" && config.Data.Database.Path == "" {
-		config.Data.Database.Path = "./data.db"
+	// 对于 SQLite，如果Path 未设置，则设置为当前目录下的 data.db
+	if config.Data.Type == "sqlite" && config.Data.Path == "" {
+		config.Data.Path = "./data.db"
 		logger.GlobalLogger.Warn("SQLite database path is not set, defaulting to ./data.db")
 	}
 }
 
 func CheckStatus() error {
 	CheckAndSetDefaultConfig()
-	switch config.Data.Database.Type {
+	switch config.Data.Type {
 	case "":
 		logger.GlobalLogger.Warn("Database type is not specified, defaulting to sqlite")
 		fallthrough
 	case "sqlite":
-		if config.Data.Database.Path == "" {
+		if config.Data.Path == "" {
 			return errors.New("SQLite database path is not set")
 		}
 	case "mysql":
-		if config.Data.Database.Addr == "" || config.Data.Database.User == "" || config.Data.Database.Password == "" || config.Data.Database.Name == "" {
+		if config.Data.Addr == "" || config.Data.Database.User == "" || config.Data.Password == "" || config.Data.Name == "" {
 			logger.GlobalLogger.Error("Missing required MySQL configuration parameters")
 			return errors.New("Missing required MySQL configuration parameters")
 		}
 	default:
-		return errors.New("Unsupported database type: " + config.Data.Database.Type)
+		return errors.New("Unsupported database type: " + config.Data.Type)
 	}
 	return nil
 }
 
 func GetDBConnection() (db *gorm.DB, err error) {
 	var dbHandler Connector
-	switch config.Data.Database.Type {
+	switch config.Data.Type {
 	case "sqlite":
-		dbHandler = NewSQLiteDB(config.Data.Database.Path)
+		dbHandler = NewSQLiteDB(config.Data.Path)
 	case "mysql":
-		dbHandler = NewMySQLDB(config.Data.Database.Addr, config.Data.Database.User, config.Data.Database.Password, config.Data.Database.Name)
+		dbHandler = NewMySQLDB(config.Data.Addr, config.Data.Database.User, config.Data.Password, config.Data.Name)
 	default:
 		logger.GlobalLogger.Error("Unsupported database type")
 		panic("Unsupported database type")
